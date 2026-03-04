9ec2d5518fd90e91bbf081f5bda8ffaf481f6cef
lucasmoura123494 <moura.rodrigues@aluno.ifsp.edu.br>
2026-03-04 19:48:56 -0300

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,45 @@
+SELECT count(*)
+FROM section;
+--2000
+
+SELECT count(*)
+FROM student;
+--30000
+
+SELECT count(*)
+FROM takes;
+--100
+
+SELECT count(*)
+FROM teaches;
+--100
+
+SELECT count(*)
+FROM time_slot;
+--20
+
+SELECT count(*)
+FROM advisor;
+--2000
+
+
+SELECT count(*)
+FROM classroom;
+--30
+
+SELECT count(*)
+FROM course;
+--200
+
+SELECT count(*)
+FROM department;
+--20
+
+SELECT count(*)
+FROM instructor;
+--50
+
+SELECT count(*)
+FROM  prereq;
+--100
+
